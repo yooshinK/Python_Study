f13db4418a8b5fdf74b5025e99526ead95d3cf11
yoo shin kim <52288459+yooshinK@users.noreply.github.com>
2020-06-25 22:54:43 +0900

    Add files via upload

---
+++ basic_math.py
@@ -0,0 +1,13 @@
+
+print(10+5)
+print(10-5)
+print(10*5)
+print(10/5)
+print(10%5)
+
+#python requires import
+import math
+print(math.ceil(2.0))
+print(math.floor(19.9))
+print(math.pow(2,4))
+print(math.pi) # = 3.141592653589793
