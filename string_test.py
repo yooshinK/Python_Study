2c91afdd8432e8026b98e1f1bca42ca611a63d15
yoo shin kim <52288459+yooshinK@users.noreply.github.com>
2020-06-26 23:20:07 +0900

    Add files via upload

---
+++ string_test.py
@@ -0,0 +1,21 @@
+print('Hello '+'python')
+print('Hello '+'python '*3)
+
+# '-', '/', '%' cause an error - but Ruby % doesn't cause an error
+#print('Hello '-'python '/3)
+
+# array puts('Hello ' [0])
+print('HelAo ' [3])
+
+#To make the first latter an Upper latter
+print('aaaaaaaaaaa'.capitalize())
+
+#To make the whole sentence Upper latters
+print('aaaaaaaaaaa'.upper())
+
+#To get the length of the sentence
+print('aaaaaaaaaaa'.__len__())
+print(len('aaaaaaaaaaaA')) # doesn't work on Ruby
+
+# To substitute
+print('Hello World'.replace('orld','Python'))
