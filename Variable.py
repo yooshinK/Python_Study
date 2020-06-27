e30c9eb02888240f93b73bff011851ca6c2f1cbc
yoo shin kim <52288459+yooshinK@users.noreply.github.com>
2020-06-27 12:35:24 +0900

    Add files via upload

---
+++ Variable.py
@@ -0,0 +1,13 @@
+x=100
+y=5
+print(x+y)
+
+title = "Photography"
+#Converting Data to Strings .__str__ or str(x)
+print("Story"+title+x.__str__())
+print("Story"+title+str(x))
+
+#Converting Strings to Numbers int(x), float(x)
+print(int("5"))
+print(float("5"))
+# print(int("5.8")) # int에 소수점있으면 에러남
