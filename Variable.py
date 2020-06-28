38d7f5603e44759c0812c54ace637cf633f84941
yoo shin kim <52288459+yooshinK@users.noreply.github.com>
2020-06-28 22:26:21 +0900

    Update Variable.py

--- Variable.py
+++ Variable.py
@@ -11,3 +11,8 @@ print("Story"+title+str(x))
 print(int("5"))
 print(float("5"))
 # print(int("5.8")) # int에 소수점있으면 에러남
+
+donation = 200
+sponsor = 20
+student = 5
+print("Amount of donations per sponsors = "+ ((donation*student)/sponsor).__str__())
