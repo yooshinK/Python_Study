7571d872d28f5a086fcb40b126cd857d94b3ec00
yoo shin kim <52288459+yooshinK@users.noreply.github.com>
2020-06-30 21:40:40 +0900

    Add files via upload

---
+++ login_function_test.py
@@ -0,0 +1,15 @@
+
+in_str = input("Please tell me your nickname!\n")
+#print(in_str.upper()+" in 10 seconds")
+
+
+input = 22
+real_yooshink = "keroro"
+real_noah = "tamama"
+
+if in_str == real_yooshink:
+    print("Hi Yooshin~")
+elif in_str == real_noah: # else if -> elif
+    print("Hi Nonah~")
+else:
+    print("who are you?")
