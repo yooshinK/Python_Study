d4cc6c74275b69a7db992650c3460b748b89e396
yoo shin kim <52288459+yooshinK@users.noreply.github.com>
2020-07-06 23:57:40 +0900

    Add files via upload

---
+++ Container.py
@@ -0,0 +1,25 @@
+# Result = <class 'str'>
+name = 'yooshink'
+print(type('yooshink'))
+print(name)
+
+# Result = <class 'list'>
+print("Names - ")
+names = ['yooshin','TEST',"Noah"]
+print(type(['yooshin','TEST',"Noah"]))
+print(type(names))
+print(names)
+print(names[0])
+print(names[1])
+print(names[2])
+
+print("A Person - ")
+person = ['yooshin',29,True]
+print(type(['yooshin',29,True]))
+print(type(person[2]))
+print(person)
+print(person[0])
+print(person[1])
+print(person[2])
+person[1] +=  1
+print(person[1])
