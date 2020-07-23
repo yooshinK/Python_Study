c2a5dd5e07c6bf2beeb05ad1c1ec904ab3316010
yoo shin kim <52288459+yooshinK@users.noreply.github.com>
2020-07-23 16:39:56 +0900

    Update container_loop.py

--- container_loop.py
+++ container_loop.py
@@ -1,8 +1,25 @@
-members = ['keroro','dororo','tamama']
+members1 = ['keroro','dororo','tamama']
+members2 = ['keroro1','dororo1','tamama1']
 
 i = 0
+j = 0
 
-while i < len(members):
-    print(members[i]+" ")
+# while
+while i < len(members1):
+    print(members1[i]+" ")
     i+=1
+    pass
+print("-----------------------------")
+
+# for with range
+for k in range(3): # k is automatically set as 0
+    print(members2[k]+" "+ str(j))
+    j += 1
+    pass
+    
+print("-----------------------------")
+# for with container
+for member in members2: # member is automatically set as 0
+    print(member+" "+ str(j))
+    j += 1
     pass
