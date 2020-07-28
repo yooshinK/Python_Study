374c23248f7c6e02c1364cfdafb9f41f6c0b40fa
yoo shin kim <52288459+yooshinK@users.noreply.github.com>
2020-07-28 22:59:12 +0900

    Add files via upload

--- def_function.py
+++ def_function.py
@@ -13,5 +13,24 @@ print("-----------------------------")
 def b3():
     print("Python b3's return")
     return 'Python bbb'
-
 print(b3())
+
+print("-----------------------------")
+#function Input X 1
+
+def c(num):
+    print("Python Function C's input")
+    print(num)
+    return num*'c'
+print(c(100))
+
+print("-----------------------------")
+#function Input X 2
+
+def alphabet(num,str):
+    print("Python Function Alphabet")
+    print(num)
+    print(str)
+    return num*str
+
+print(alphabet(100,'K'))
