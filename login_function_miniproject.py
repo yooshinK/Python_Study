fef5bab357da732b87c92db930bbfffab90aeff7
yoo shin kim <52288459+yooshinK@users.noreply.github.com>
2020-07-29 21:04:48 +0900

    with Function Version 1st

--- login_function_miniproject.py
+++ login_function_miniproject.py
@@ -1,23 +1,33 @@
-real_members = ['yooshin','noah','keroro','tamama','dororo','kiroro']
-real_id = "lifeandstory"
-real_pwd = "story"
-pwd_count = 5
-logged_in = False
-#Input
+#functions
+def login(user_id):
+    real_members = ['yooshin','noah','keroro','tamama','dororo','kiroro','lifeandstory']
+    real_pwd = "story"
+    pwd_count = 5
+    logged_in = False
+
+    for member in real_members:
+        if member == user_id:
+            while pwd_count>0:
+                input_pwd = input("Please input your password\n")
+                if real_pwd == input_pwd:
+                    logged_in = True
+                    return True
+                else:
+                    print("Wrong Password! Count:"+str(pwd_count-1))
+                    pwd_count -= 1
+            if pwd_count==0:
+               return False
+
+    if (logged_in == False) and (pwd_count==5):
+        return False
+
+#Main
 input_id = input("Please input your id\n")
-for member in real_members:
-    if member == input_id:
-        while pwd_count>0:
-            input_pwd = input("Please input your password\n")
-            if real_pwd == input_pwd:
-                print("Welcome! "+ member)
-                logged_in = True
-                break
-            else:
-                print("Wrong Password! Count:"+str(pwd_count))
-                pwd_count -= 1
-if (logged_in == False) and (pwd_count==5) :
-    print("Wrong ID")
+
+if login(input_id):
+  print("Welcome! "+ input_id)
+else:
+  print("Wrong ID or Wrong Password!")
 
 # #Password function 2
 # print("Password function 2")
