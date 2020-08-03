e74b2d8b3f19abad636dfb88a25ec053b67b8af1
yoo shin kim <52288459+yooshinK@users.noreply.github.com>
2020-08-04 00:04:35 +0900

    Add files via upload

---
+++ module_test.py
@@ -0,0 +1,7 @@
+import math # more module info: docs.python.org/3/library/math.html
+print(math.ceil(2.1)) # Result = 3
+print(math.ceil(2.9)) # Result = 3
+print(math.floor(2.1)) # Result = 2
+print(math.floor(2.9)) # Result = 2
+print(math.sqrt(15)) # Result = 3.872983346207417
+print(3.872983346207417*3.872983346207417) # Result =
