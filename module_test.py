26382224f7076cf3bfbd25d4d21847636f0885ec
yoo shin kim <52288459+yooshinK@users.noreply.github.com>
2020-08-05 23:25:05 +0900

    Add files via upload

--- module_test.py
+++ module_test.py
@@ -4,4 +4,12 @@ print(math.ceil(2.9)) # Result = 3
 print(math.floor(2.1)) # Result = 2
 print(math.floor(2.9)) # Result = 2
 print(math.sqrt(15)) # Result = 3.872983346207417
-print(3.872983346207417*3.872983346207417) # Result =
+print(3.872983346207417*3.872983346207417) # Result = 15.000000000000002
+
+print("---------------------------------------")
+import module_test2
+def a():
+    return 'bB'
+
+print(a())
+print(module_test2.a())
