ec178db7dbd675c8245466a9d012d87715830c89
yoo shin kim <52288459+yooshinK@users.noreply.github.com>
2020-08-08 11:27:47 +0900

    Add files via upload

---
+++ authentication_module.py
@@ -0,0 +1,21 @@
+def login(user_id):
+    real_members = ['yooshin','noah','keroro','tamama','dororo','kiroro','lifeandstory']
+    real_pwd = "story"
+    pwd_count = 5
+    logged_in = False
+
+    for member in real_members:
+        if member == user_id:
+            while pwd_count>0:
+                input_pwd = input("Please input your password\n")
+                if real_pwd == input_pwd:
+                    logged_in = True
+                    return True
+                else:
+                    print("Wrong Password! Count:"+str(pwd_count-1))
+                    pwd_count -= 1
+            if pwd_count==0:
+               return False
+
+    if (logged_in == False) and (pwd_count==5):
+        return False
