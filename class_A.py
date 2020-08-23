d55cd0350e917786375f6617642d24fd964d2c00
yoo shin kim <52288459+yooshinK@users.noreply.github.com>
2020-08-23 23:33:53 +0900

    Update class_A.py

--- class_A.py
+++ class_A.py
@@ -1,8 +1,12 @@
 class Cal(object):
     def __init__(self, a, b): #생성자, Constructor -> 자동호출
-        print(a,b)
-    def add:
-        return a+b
+        print(a,b) #a and b are local variables
+        self.a = a
+        self.b = b
+    def add(self):
+        return self.a+self.b # self. are instance variables
+    def subtract(self):
+        return self.a-self.b
 
 c1 = Cal(10,7)
 print(c1.add())
