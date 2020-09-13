a0f646a16e7825edb5ac9aa4ed3e4825bbf31cd2
yoo shin kim <52288459+yooshinK@users.noreply.github.com>
2020-09-13 09:27:09 +0900

    Update override.py

--- override.py
+++ override.py
@@ -4,8 +4,12 @@ class Family:
 
 class K_Family(Family):
     def me(self):
-        super().me()  # call parent class's me()
+        print(super().me()) # call parent class's me()
         return super().me() + ' and ' + 'I am Child'
 
 test = K_Family()
 print(test.me())
+
+# Result
+# I am parent
+# I am parent and I am Child
