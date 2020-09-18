8559de253b96edd6b7ea3be25e05cca3415bcc3f
yoo shin kim <52288459+yooshinK@users.noreply.github.com>
2020-09-18 22:49:22 +0900

    Update multi_inheritance.py

--- multi_inheritance.py
+++ multi_inheritance.py
@@ -1,23 +1,14 @@
-class C1():
-    def c1_m(self):
-        print("c1_m")
-    def m(self):
-        print("C1 m")
- 
-class C2():
-    def c2_m(self):
-        print("c2_m")
-    def m(self):
-        print("C2 m")
- 
-class C3(C2, C1):
-    def m(self):
-        print("C3 m")
- 
-c = C3()
-c.c1_m()
-c.c2_m()
-c.m()
-print(C3.__mro__)
+class Class_TEST1():
+    def C_T1_method(arg):
+        print("This is Class_TEST1's Method")
 
-#Test
+class Class_TEST2():
+    def C_T2_method(arg):
+        print("This is Class_TEST2's Method")
+
+class Class_TEST3(Class_TEST1,Class_TEST2): #다중상속
+    pass # 이 클래스, 함수는 기능이 없다는 뜻으로 항상 적어주어야한다.
+
+c = Class_TEST3()
+c.C_T1_method()
+c.C_T2_method()
