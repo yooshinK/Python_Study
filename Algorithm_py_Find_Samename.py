f61afee92ab05cc8f6608cb2152d445ae4d35d19
yoo shin kim <52288459+yooshinK@users.noreply.github.com>
2020-09-30 21:48:52 +0900

    Update Algorithm_py_Find_Samename.py

--- Algorithm_py_Find_Samename.py
+++ Algorithm_py_Find_Samename.py
@@ -7,7 +7,8 @@ def find_Samename(array_values):
     name_set = set()
     length = len(array_values)
     print(length)
-    count = 0
+    global count # 전역변수 선언 방법
+    count = 0 # 전역변수 선언 방법
     for i in range(0, length-1):
         s = array_values[i]
         print("FOR 1 in it / i is - "+ i.__str__() + ", " + s.__str__())
@@ -15,10 +16,10 @@ def find_Samename(array_values):
             print("FOR 2 in it / j is - "+ j.__str__())
             if s == array_values[j]:
                 print("in it? IF ")
-                print(s)
+                count += 1
+                print(s, count)
                 name_set.add(s)
                 s = array_values[i]
-                count += 1
         print("-------FOR 1--------")
     # print("Max Id is " + max_idx.__str__())
     return name_set
@@ -29,6 +30,7 @@ print("------Find Same Name - For--------")
 print("Same name set", end =" ") # on the same line printing
 # sys.stdout.write("TEST") # on the same line printing
 print(find_Samename(name_list))
+print(count)
 #-------------------------------------
 
 # # # Result
@@ -50,7 +52,7 @@ print(find_Samename(name_list))
 # FOR 2 in it / j is - 3
 # FOR 2 in it / j is - 4
 # in it? IF
-# kiroro
+# kiroro 1
 # FOR 2 in it / j is - 5
 # FOR 2 in it / j is - 6
 # FOR 2 in it / j is - 7
@@ -65,7 +67,7 @@ print(find_Samename(name_list))
 # FOR 2 in it / j is - 7
 # FOR 2 in it / j is - 8
 # in it? IF
-# dororo
+# dororo 2
 # FOR 2 in it / j is - 9
 # -------FOR 1--------
 # FOR 1 in it / i is - 3, tamama
@@ -76,7 +78,7 @@ print(find_Samename(name_list))
 # FOR 2 in it / j is - 8
 # FOR 2 in it / j is - 9
 # in it? IF
-# tamama
+# tamama 3
 # -------FOR 1--------
 # FOR 1 in it / i is - 4, kiroro
 # FOR 2 in it / j is - 5
@@ -88,17 +90,17 @@ print(find_Samename(name_list))
 # FOR 1 in it / i is - 5, kururu
 # FOR 2 in it / j is - 6
 # in it? IF
-# kururu
+# kururu 4
 # FOR 2 in it / j is - 7
 # in it? IF
-# kururu
+# kururu 5
 # FOR 2 in it / j is - 8
 # FOR 2 in it / j is - 9
 # -------FOR 1--------
 # FOR 1 in it / i is - 6, kururu
 # FOR 2 in it / j is - 7
 # in it? IF
-# kururu
+# kururu 6
 # FOR 2 in it / j is - 8
 # FOR 2 in it / j is - 9
 # -------FOR 1--------
@@ -109,4 +111,5 @@ print(find_Samename(name_list))
 # FOR 1 in it / i is - 8, dororo
 # FOR 2 in it / j is - 9
 # -------FOR 1--------
-# {'kiroro', 'kururu', 'tamama', 'dororo'}
+# {'dororo', 'kururu', 'tamama', 'kiroro'}
+# 6
