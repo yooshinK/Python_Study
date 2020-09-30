f9f6b71c796659c4155af040477403faf461970c
yoo shin kim <52288459+yooshinK@users.noreply.github.com>
2020-09-30 21:26:57 +0900

    Update Algorithm_py_Find_Samename.py

--- Algorithm_py_Find_Samename.py
+++ Algorithm_py_Find_Samename.py
@@ -1,19 +1,22 @@
 import sys
 #-------------------------------------
-name_list = ["keroro","kiroro","dororo","tamama","kiroro","kururu"]
+name_list = ["keroro","kiroro","dororo","tamama","kiroro","kururu","dororo"]
 #-------------------------------------
 def find_Samename(array_values):
+    # print("in it? 1")
     name_set = set()
-    name_set.add("test")
     length = len(array_values)
-    s = array_values[0]
     count = 0
-    for i in range(0, length-1):
-        if s == array_values[i+1]:
-            name_set.add(s)
-            count += 1
-            print(s)
-            s = array_values[i]
+    for i in range(0, length-2):
+        # print("FOR 1 in it?")
+        s = array_values[i]
+        for j in range(i+1, length-1):
+            # print("FOR 2 in it?")
+            if s == array_values[j]:
+                name_set.add(s)
+                s = array_values[i]
+                count += 1
+                # print("in it? IF ")
 
     # print("Max Id is " + max_idx.__str__())
     return name_set
