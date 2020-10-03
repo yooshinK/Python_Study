f041d07c649ebaf37fc9b42d684f2a99dae67404
yoo shin kim <52288459+yooshinK@users.noreply.github.com>
2020-10-03 19:13:34 +0900

    Update Algorithm_py_Find_Max_Num.py
    find_max_num_recursive(array_values, n) 함수 추가

--- Algorithm_py_Find_Max_Num.py
+++ Algorithm_py_Find_Max_Num.py
@@ -1,48 +1,6 @@
 import math
 #-------------------------------------
-num_list = [17,92,18,33,58,700,33,42]
-#-------------------------------------
-s = 0
-for i in num_list:
-    if s <= i:
-        s = i
-    else:
-        pass
-
-print("------Find Max Num - For--------")
-print(s)
-#-------------------------------------
-# # Result
-# ------Find Max Num - For--------
-# 700
-
-#-------------------------------------
-import math
-#-------------------------------------
-num_list = [17,92,18,33,58,70,330,420]
-#-------------------------------------
-def find_max_num_for(array_values):
-    n = len(array_values)
-    s = array_values[0]
-    for i in range(1, n):
-        if s <= array_values[i]:
-            s = array_values[i]
-    return s
-        # else:
-        #     pass
-
-print("------Find Max Num - For--------")
-print(find_max_num_for(num_list))
-#-------------------------------------
-
-# # Result
-# ------Find Max Num - For--------
-# 420
-
-
-import math
-#-------------------------------------
-num_list = [17,92,18,33,58,70,330,420]
+num_list = [17,902,18,33,58,700,330,4550]
 #-------------------------------------
 def find_max_num_for(array_values):
     n = len(array_values)
@@ -54,15 +12,28 @@ def find_max_num_for(array_values):
             max_idx = i
     print("Max Id is " + max_idx.__str__())
     return s, max_idx
-        # else:
-        #     pass
+#-------------------------------------
+def find_max_num_recursive(array_values, n):
+    if n > 0:
+        Max_num = find_max_num_recursive(array_values, n-1)
+        if array_values[n] <= Max_num:
+            return Max_num
+        else:
+            return array_values[n]
+    elif n == 0:
+        return array_values[0]
+
+#-------------------------------------
 
 print("------Find Max Num - For--------")
 print(find_max_num_for(num_list))
+print("------Find Max Num - Recursive--------")
+print(find_max_num_recursive(num_list, len(num_list)-1))
 #-------------------------------------
 
 # # Result
 # ------Find Max Num - For--------
-# Max Id is 7
-# (420, 7)
-
+# Max Id is 1
+# (902, 1)
+# ------Find Max Num - Recursive--------
+# 902
