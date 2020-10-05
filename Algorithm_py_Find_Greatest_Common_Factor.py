7518089fe915db78daf0b55f473c533b6eccb997
yoo shin kim <52288459+yooshinK@users.noreply.github.com>
2020-10-05 23:06:42 +0900

    Update Algorithm_py_Find_Greatest_Common_Factor.py

--- Algorithm_py_Find_Greatest_Common_Factor.py
+++ Algorithm_py_Find_Greatest_Common_Factor.py
@@ -1,23 +1,28 @@
 import math
 #-------------------------------------
-def find_max_num_for(array_values):
-    n = len(array_values)
-    s = array_values[0]
-    max_idx = 0
-    for i in range(1, n):
-        if s <= array_values[i]:
-            s = array_values[i]
-            max_idx = i
-    print("Max Id is " + max_idx.__str__())
-    print("Max Number is ", s)
+def find_GCF(v1,v2):
+    if v1 == v2:
+        return v1
+        
+    elif v1 < v2:
+        i = v1
+        for i in range(i, 1):
+            if (v2 / i) == 0:
+                return i
+            else:
+                i -= 1
+    else:
+        i = v2
+        for i in range(i, 1):
+            if (v1 / i) == 0:
+                return i
+            else:
+                i -= 1
 #-------------------------------------
-num_list = []
-num = int(input("Enter how many elements you want"))
-print('Enter numbers in array: ')
-for i in range(0,num):
-    # a = int(input("num :"))
-    num_list.append(int(input("num :")))
-find_max_num_for(num_list)
-print('ARRAY: ', num_list)
+num1 = int(input("Enter the first num"))
+num2 = int(input("Enter the second num"))
+
+print('The Greatest Common Factor is')
+find_GCF(num1, num2)
 #-------------------------------------
 # # Resutl
