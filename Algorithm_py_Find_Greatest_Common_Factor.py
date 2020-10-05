f8ada4547fab56d6f178ff4e122ce006049b783b
yoo shin kim <52288459+yooshinK@users.noreply.github.com>
2020-10-05 23:36:23 +0900

    Update Algorithm_py_Find_Greatest_Common_Factor.py

--- Algorithm_py_Find_Greatest_Common_Factor.py
+++ Algorithm_py_Find_Greatest_Common_Factor.py
@@ -1,6 +1,6 @@
 import math
 #-------------------------------------
-def find_GCF(v1,v2):
+def find_GCF1(v1,v2):
     if v1 == v2:
         return v1
 
@@ -19,10 +19,18 @@ def find_GCF(v1,v2):
             else:
                 s -= 1
 #-------------------------------------
+def find_GCF2(v1,v2):
+    s = min(v1,v2)
+    while True:
+        if (v1 % s) == 0 and (v2 % s) == 0 :
+            return s
+        else:
+            s -= 1
+#-------------------------------------
 num1 = int(input("Enter the first num - "))
 num2 = int(input("Enter the second num - "))
 
 print("The Greatest Common Factor is", end =" ")
-print(find_GCF(num1, num2))
+print(find_GCF2(num1, num2))
 #-------------------------------------
 # # Resutl
