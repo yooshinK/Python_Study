eaba0195ae632d3ecf4ab0da3199d892ecc99ed6
yoo shin kim <52288459+yooshinK@users.noreply.github.com>
2020-10-07 21:23:54 +0900

    Update Algorithm_py_Euclidean_Algorithm-Find_Greatest_Common_Factor.py

--- Algorithm_py_Euclidean_Algorithm-Find_Greatest_Common_Factor.py
+++ Algorithm_py_Euclidean_Algorithm-Find_Greatest_Common_Factor.py
@@ -8,7 +8,10 @@ def Euclidean_algorithm(v1,v2):
     elif min_idx != 0:
         return Euclidean_algorithm(min_idx ,max_idx % min_idx)
 #-------------------------------------
+
+#-------------------------------------
 def Euclidean_algorithm(a,b):
+    print("gcd: - ", a, b)
     if b == 0:
         return a
     return Euclidean_algorithm(b, (a % b))
@@ -19,3 +22,16 @@ num2 = int(input("Enter the second num - "))
 print("The Greatest Common Factor - Euclidean Algorithm", end =" - ")
 print(Euclidean_algorithm(num1, num2))
 #-------------------------------------
+# Result:
+# Enter the first num - 1760
+# Enter the second num - 2740
+# The Greatest Common Factor - Euclidean Algorithm - gcd: -  1760 2740
+# gcd: -  2740 1760
+# gcd: -  1760 980
+# gcd: -  980 780
+# gcd: -  780 200
+# gcd: -  200 180
+# gcd: -  180 20
+# gcd: -  20 0
+# 20
+
