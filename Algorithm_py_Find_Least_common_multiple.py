2202eb686c0926633fc582ad5fde741028a4f44b
yoo shin kim <52288459+yooshinK@users.noreply.github.com>
2020-10-11 21:17:32 +0900

    Update Algorithm_py_Find_Least_common_multiple.py

--- Algorithm_py_Find_Least_common_multiple.py
+++ Algorithm_py_Find_Least_common_multiple.py
@@ -1,25 +1,12 @@
 import math
 #-------------------------------------
-def find_LCM1(v1,v2):
-    if v1 == v2:
-        return v1
-
-    elif v1 < v2:
-        s = v1
-        while s >= 1:
-            if (v2 % s) == 0:
-                return s
-            else:
-                s -= 1
+def find_LCM(v1,v2):
+    if v1 != 0 and v2 != 0:
+        return (v1*v2) // find_CGF(v1,v2)
     else:
-        s = v2
-        while s >= 1:
-            if (v1 % s) == 0:
-                return s
-            else:
-                s -= 1
+        return 0
 #-------------------------------------
-def find_LCM2(v1,v2):
+def find_CGF(v1,v2):
     s = min(v1,v2)
     while True:
         if (v1 % s) == 0 and (v2 % s) == 0 :
@@ -27,10 +14,11 @@ def find_LCM2(v1,v2):
         else:
             s -= 1
 #-------------------------------------
-num1 = int(input("Enter the first num - "))
-num2 = int(input("Enter the second num - "))
+# num1 = int(input("Enter the first num - "))
+# num2 = int(input("Enter the second num - "))
 
-print("The Least Common Multiple Factor - Function 1", end =" ")
-print(find_LCM1(num1, num2))
+print("The Least Common Multiple Factor - Function", end =" : ")
+print(find_LCM(72, 192))
 #-------------------------------------
-# # Resutl
+# # Result
+# The Least Common Multiple Factor - Function 576
